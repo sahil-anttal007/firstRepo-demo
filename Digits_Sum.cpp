e86ed2669c6cb1f0417fb0f3c4a4994295c3d930
sahil-anttal007 <anttalsahil8@gmail.com>
2026-09-12 10:02:48 +0530

    Adding Sum of Digits.

---
+++ Digits_Sum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int SumOfDigits(int num){
+    int digSum = 0;
+    while(num > 0){
+        int lastdig = num % 10;
+        num /= 10;
+        digSum += lastdig;
+    }
+    return digSum;
+}
+int main(){
+    cout<<"Sum of digits = "<<SumOfDigits(73295)<<endl;
+    return 0;
+}
