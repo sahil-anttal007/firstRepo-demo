d9cf6c7faab3f33c153298f481be0275d83335ae
sahil-anttal007 <anttalsahil8@gmail.com>
2026-09-12 09:07:15 +0530

    Merge branch 'main' of https://github.com/sahil-anttal007/firstRepo-demo

--- hello.cpp
+++ hello.cpp
@@ -1,17 +1,22 @@
 #include<iostream>
 using namespace std;
-int SumOfDigits(int n){
-    int digsum = 0;
-    while(n>0){
-        int lastDig = n % 10;
-        n /= 10;
-        digsum += lastDig;
+int factorial(int n){
+    int fact = 1;
+    for(int i = 1; i <= n; i++){
+        fact *= i;
     }
-    return digsum;
+    return fact;
+}
+int nCr(int n, int r){
+    int fact_nCr = factorial(n) / (factorial(r) * factorial(n - r));
+    return fact_nCr;
 }
 int main(){
-    int n;
-    cin >> n;   
-    cout << "Sum of digits of " << n << " is " << SumOfDigits(n) << endl;
+    int n, r;
+    cout << "Enter n for nCr calculation: ";
+    cin >> n;
+    cout << "Enter r for nCr calculation: ";
+    cin >> r;
+    cout << nCr(n, r) << endl;
     return 0;
 }
